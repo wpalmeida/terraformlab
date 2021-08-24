424d119bda21fe508b21e8747618aab18002318f
Willian Pereira <willian.pereira@inventcloud.com.br>
2021-08-24 17:36:18 -0300

    a

--- bootvolume.tf
+++ bootvolume.tf
@@ -1,8 +1,8 @@
 resource "oci_core_boot_volume" "test_boot_volume" {
     compartment_id = var.compartment_ocid
     source_details {
-        #id = "ocid1.bootvolumebackup.oc1.sa-saopaulo-1.abtxeljreapvn4nknubhpyurcv5wexun7z2j4m65zoaipgbhvgigyuwm4x2a"
-        id = data.oci_core_boot_volume_backups.test_boot_volume_backups.id
+        id = "ocid1.bootvolumebackup.oc1.sa-saopaulo-1.abtxeljreapvn4nknubhpyurcv5wexun7z2j4m65zoaipgbhvgigyuwm4x2a"
+        #id = data.oci_core_boot_volume_backups.test_boot_volume_backups.id
         #id = data.oci_core_boot_volume_backups.test_boot_volume_backups.boot_volume_backups.id
         type = "bootVolumeBackup"
     }
