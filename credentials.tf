caa83d102fe1fc1c1a15e3cd434da7eacf032698
Willian Pereira <willian.pereira@inventcloud.com.br>
2021-08-24 11:14:47 -0300

    Comitando os arquivos main e credentials

---
+++ credentials.tf
@@ -0,0 +1,15 @@
+variable "tenancy_ocid" {
+  default = "ocid1.tenancy.oc1..aaaaaaaaqd2wphhup27uq3bfcw5dt4udtoo3lucpdum3vnit7mg2syegpmka"
+}
+
+variable "user_ocid" {
+  default = "ocid1.user.oc1..aaaaaaaay4glds2tnf7isf4jojgkpgk7yrynrscmm3g6r22pusgtcw3xbzza"
+}
+
+variable "fingerprint" {
+  default = "f8:d7:c8:68:d6:f2:7f:a5:9d:94:16:73:ad:83:35:ed"
+}
+
+variable "private_key_path" {
+  default = "-08-17-13-27.pem"
+}
